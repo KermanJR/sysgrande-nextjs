d89b3a890f6660dd02996073fc7991541e00d722
Kerman Pereira <63175481+KermanJR@users.noreply.github.com>
2024-10-25 10:51:17 -0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 import { AuthProvider } from "@/app/context/AuthContext";
 import "./globals.css";
 import Theme from "@/app/theme/theme";
-import 'leaflet/dist/leaflet.css';
 import { CartProvider } from "@/app/context/CartContext";
 import { SnackbarProvider } from "notistack";
 
