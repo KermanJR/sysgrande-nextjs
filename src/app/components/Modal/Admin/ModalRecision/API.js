319164d36ce68729350e1a4745e9d1d747a4a5fe
Kerman Pereira <kermanpereira@gmail.com>
2024-12-16 10:44:41 -0300

    Modal URL API Changed

--- src/app/components/Modal/Admin/ModalRecision/API.js
+++ src/app/components/Modal/Admin/ModalRecision/API.js
@@ -1,7 +1,10 @@
+const URL = 'https://sysgrande-nodejs.onrender.com/api/'
+
+
 export const fetchedEmployeesByCompany = async (companyName) => {
     try {
       const response = await fetch(
-        `http://localhost:5000/api/employees?company=${companyName}`,
+        `${URL}employees?company=${companyName}`,
         {
           method: "GET",
           headers: {
@@ -23,7 +26,7 @@ export const fetchedEmployeesByCompany = async (companyName) => {
   export const createExpense = async (expenseData) => {
     try {
       const response = await fetch(
-        "http://localhost:5000/api/expenses/vacation",
+        `${URL}expenses/vacation`,
         {
           method: "POST",
           body: expenseData,
@@ -45,7 +48,7 @@ export const fetchedEmployeesByCompany = async (companyName) => {
   export const updateExpense = async (expenseData, id) => {
     try {
       const response = await fetch(
-        `http://localhost:5000/api/expenses/vacation/${id}`,
+        `${URL}expenses/vacation/${id}`,
         {
           method: "PUT",
           body: expenseData, // Passando o FormData diretamente como body
